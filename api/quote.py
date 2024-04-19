7a94d4c8c9c7909b122e00857f71f74b724d099a
Michelle Welcks <michelle.welcks@xebia.com>
2024-04-19 12:42:42 -0400

    CLN: Reformat file

--- api/quote.py
+++ api/quote.py
@@ -1,12 +1,12 @@
 from flask import jsonify, request
 import requests
 
-def generate_quote(request, client, deployment_name):
 
+def generate_quote(request, client, deployment_name):
     data = request.json
-    userPrompt = data.get('prompt')
+    userPrompt = data.get("prompt")
     systemPrompt = "You are a quote generator. Create an inspirational quote based on the user input."
-    
+
     if userPrompt:
 
         completion = client.chat.completions.create(
@@ -22,13 +22,12 @@ def generate_quote(request, client, deployment_name):
                 },
             ],
         )
-        
-        print (completion.model_dump_json(indent=2))
-        
-        quote = completion.choices[0].message.content     
 
-        return jsonify({'quote': quote})
+        print(completion.model_dump_json(indent=2))
+
+        quote = completion.choices[0].message.content
+
+        return jsonify({"quote": quote})
 
     else:
-        return jsonify({'error': 'User prompt parameter is missing.'})
-
+        return jsonify({"error": "User prompt parameter is missing."})
